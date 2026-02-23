76e60695b036e1c2e5516a7d048fa938ba8f8ff7
baibrea <breannacbai@gmail.com>
2026-02-22 22:24:43 -0500

    Foundation for Firebase auth (not tested)

---
+++ src/lib/auth.ts
@@ -0,0 +1,40 @@
+import { createUserWithEmailAndPassword,signInWithEmailAndPassword, User } from "firebase/auth";
+
+import { doc, setDoc } from "firebase/firestore";
+import { auth, db } from "./firebase";
+
+export async function registerUser(
+    email: string,
+    password: string
+): Promise<User> {
+    const userCredential = await createUserWithEmailAndPassword(
+        auth,
+        email,
+        password
+    );
+
+    const user = userCredential.user;
+
+    await setDoc(doc(db, "users", user.uid), {
+        uid: user.uid,
+        email: user.email,
+        name,
+        createdAt: new Date(),
+        role: "user",
+    });
+
+    return user;
+}
+
+export async function loginUser(
+    email: string,
+    password: string
+): Promise<User> {
+    const userCredential = await signInWithEmailAndPassword(
+        auth,
+        email,
+        password
+    );
+
+    return userCredential.user;
+}
